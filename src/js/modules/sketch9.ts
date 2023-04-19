9fbff72b6ce408084d1f0e6a93580e60a9d71047
ryota ohashi <ryota.812.skt@gmail.com>
2023-04-19 22:55:54 +0900

    modified

--- src/js/modules/sketch9.ts
+++ src/js/modules/sketch9.ts
@@ -1,80 +1,37 @@
 import p5 from "p5";
 
 const sketch = (p: p5) => {
-  const bubbles: Bubble[] = [];
-  const numBubbles = 50;
-
-  class Bubble {
-    x: number;
-    y: number;
-    radius: number;
-    color: p5.Color;
-    xNoise: number;
-    yNoise: number;
-    rNoise: number;
-
-    constructor() {
-      this.x = p.random(p.width);
-      this.y = p.random(p.height);
-      this.radius = p.random(20, 100);
-      this.color = p.color(
-        p.random(100, 255),
-        p.random(100, 255),
-        p.random(100, 255),
-        p.random(100, 200)
-      );
-      this.xNoise = p.random(1000);
-      this.yNoise = p.random(1000);
-      this.rNoise = p.random(1000);
-    }
-
-    display() {
-      p.push();
-      p.translate(this.x, this.y);
-      const angle = p.map(p.noise(this.xNoise), 0, 1, 0, p.TWO_PI);
-      const r = p.map(p.noise(this.rNoise), 0, 1, -20, 20);
-      p.rotate(angle);
-      p.fill(this.color);
-      p.noStroke();
-      p.ellipse(0, 0, this.radius + r);
-      p.pop();
-    }
-
-    move() {
-      this.x += p.map(p.noise(this.xNoise), 0, 1, -1, 1);
-      this.y += p.map(p.noise(this.yNoise), 0, 1, -1, 1);
-      this.xNoise += 0.01;
-      this.yNoise += 0.01;
-      this.rNoise += 0.01;
-      this.wrap();
-    }
-
-    wrap() {
-      if (this.x < -this.radius) {
-        this.x = p.width + this.radius;
-      } else if (this.x > p.width + this.radius) {
-        this.x = -this.radius;
-      }
-      if (this.y < -this.radius) {
-        this.y = p.height + this.radius;
-      } else if (this.y > p.height + this.radius) {
-        this.y = -this.radius;
-      }
-    }
-  }
+  let W: number;
+  let D: any;
+  let C: any;
+  let R: any;
 
   p.setup = () => {
-    p.createCanvas(p.windowWidth, p.windowHeight);
-    for (let i = 0; i < numBubbles; i++) {
-      bubbles.push(new Bubble());
-    }
+    p.createCanvas(W = 800, W);
+    D = p.drawingContext;
+    C = p.circle;
+    R = p.random;
+    p.noFill();
+    p.colorMode(p.HSL, 360, 100, 100, 1);
+    p.randomSeed(42); // ランダムシードを設定
   };
 
   p.draw = () => {
-    p.background(0);
-    for (const bubble of bubbles) {
-      bubble.move();
-      bubble.display();
+    p.clear(0,0,0,0);
+    for (let j = 90; j > 0; j--) {
+      let f = "";
+      for (let i = 2; i < 8; i++) {
+        f += `drop-shadow(-${i}px -${i}px hsla(${160 + j}, 99%, 70%, 1))`;
+      }
+      D.filter = f;
+      D.clip();
+      const x = R(W);
+      const y = R(W);
+      const d = R(9, 130);
+      p.push();
+      p.stroke("");
+      C(x, y, d);
+      p.pop();
     }
   };
 };
